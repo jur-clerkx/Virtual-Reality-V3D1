6be2c5194384a0e5401fb507067fbe969545b9e5
Sandor Gijsbers <sandor.gijsbers@live.nl>
2016-09-14 11:48:39 +0200

    Tutorial textures

---
+++ Week 2/Practicum B/Textures.py
@@ -0,0 +1,52 @@
+﻿import viz
+import vizmat
+
+viz.setMultiSample(4)
+viz.fov(60)
+viz.clearcolor (viz.SKYBLUE)
+viz.collision(viz.ON)
+
+viz.go (viz.FULLSCREEN)
+
+#Load texture
+pic = viz.addTexture('lake3.jpg')
+pic.wrap(viz.WRAP_T, viz.CLAMP_TO_BORDER)
+pic.wrap(viz.WRAP_S, viz.MIRROR)
+
+#Create surface to wrap the texture on
+quad = viz.addTexQuad()
+quad.setPosition(-.75,2,3) #put quad in view
+
+#Wrap texture on quad
+quad.texture(pic)
+
+#Create new texquad
+quad2 = viz.addTexQuad()
+quad2.setPosition(.75,2,3)
+quad2.texture(pic)
+
+matrix = vizmat.Transform()
+#Scale down texture by scaling up the texquad's texture coordinates
+matrix.setScale(1.5,1.5,1)
+#Move the (0,0) texture coordinate up and to the right by subtracting
+matrix.setTrans([-.25,-.25,1])
+quad2.texmat(matrix)
+
+#Make background wall
+WALL_SCALE = [10,3,1]
+
+wall = viz.addTexQuad()
+wall.setPosition(0,2,3)
+wall.zoffset(1) #avoid zfighing, make wall appear behind pictures
+wall.setScale( WALL_SCALE )
+
+#Apply nice repeating brick texture
+matrix = vizmat.Transform()
+matrix.setScale( WALL_SCALE )
+wall.texmat ( matrix )
+
+bricks = viz.addTexture('brick.jpg')
+bricks.wrap(viz.WRAP_T, viz.REPEAT)
+bricks.wrap(viz.WRAP_S, viz.REPEAT)
+
+wall.texture(bricks)
